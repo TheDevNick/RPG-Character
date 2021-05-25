b82ab77e4bf98a933f20f2174eaa58588b94c287
Nick Hollins <nholl22@wgu.edu>
2021-05-25 16:16:58 -0400

    manipulating JS objects!

--- js/main.js
+++ js/main.js
@@ -12,6 +12,45 @@ console.log(`Meet ${aurora.name}, she has ${aurora.health} health points and a s
 aurora.health -= 20;
 console.log(`${aurora.name} was hit by a fire arrow and now she has only ${aurora.health} health points`);
 
+// Adding a method to a RPG object
+
+const kimfer = {
+    name: 'Kimfer',
+    health: 200,
+    strength: 50,
+    xp: 0,
+    //returning kimfer character description
+    describeKimfer() {
+        return `Meet ${this.name}! He has ${this.health} health points and a strength of ${this.strength}. He have ${this.xp} experience points`;
+    }
+};
+/* a way to describe the object kimfer 
+
+console.log(`Meet ${kimfer.name}! He has ${kimfer.health} health points and a strength of ${kimfer.strength}`);
+
+*/
+
+
+/*
+a better way to describe the object kimfer
+Return the character description
+function describe(character) {
+    return `Meet ${kimfer.name}! He has ${kimfer.health} health points and a strength of ${kimfer.strength}`;
+}
+console.log(describe(kimfer));
+
+THIS IS PROCEDURAL PROGRAMMING NOT OOP! 
+*/
+
+// The best way to describe the object kimfer (in my opinion it's cleaner and more organized)
+
+console.log(kimfer.describeKimfer());
+
+// kimfer learned a new attack and a new skill
+kimfer. xp += 75;
+console.log(kimfer.describeKimfer()); //xp is now 75
+
+
 
 
 
